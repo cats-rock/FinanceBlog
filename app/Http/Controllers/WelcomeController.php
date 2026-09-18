d2d7fb92150e3c4f82fbd19fb2a9079de1d6d438
vv <netwvk@gmail.com>
2026-09-18 12:23:01 +0200

    Added more notes for Refactor homepage route to use WelcomeController commit

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,11 +1,18 @@
 <?php
+
+// This namespace tells PHP that the controller belongs to the application's controllers folder.
 namespace App\Http\Controllers; // calss belongs to namespace App\Http\Controllers
+
+// Request represents data sent by the browser. It is imported here but is not used yet.
 use Illuminate\Http\Request;
+
+// This controller receives homepage requests from the route in routes/web.php.
 class WelcomeController extends Controller
 {
+    // The route calls this public method when someone visits the homepage.
     public function index()
     {
+        // The response flow is: route -> controller -> welcome Blade view -> browser.
         return view('welcome'); // welcome is the filename shortcut. Laravel opens: resources/views/welcome.blade.php
-
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+// Import the controllers so their short class names can be used in the routes below.
 use App\Http\Controllers\Userzone\ProfileController;
 use App\Http\Controllers\WelcomeController;
+
+// Import Laravel's Route facade, which is used to define the application's URLs.
 use Illuminate\Support\Facades\Route;
 
+// Homepage flow: GET request to / -> WelcomeController -> index() -> welcome view.
 Route::get('/', [WelcomeController::class, 'index'])->name('home'); // When the get request is made to the root URL, the index method of the WelcomeController class is called.
 
 Route::get('/dashboard', function () {
